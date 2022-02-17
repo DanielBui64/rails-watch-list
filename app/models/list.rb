4e07d2eda315101be13aa551404f219f8613d704
daniel bui <daniel-bui@hotmail.com>
2022-02-17 16:44:40 -0500

    rails watch list

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,3 +1,5 @@
 class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
+  validates :name, uniqueness: true
+  #validates :name, allow_blank: false
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,7 @@
 class Movie < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
+
+  #validates :overview, allow_blank: false
+  validates :overview, presence: true
+  validates :title, uniqueness: true, presence: true
 end
